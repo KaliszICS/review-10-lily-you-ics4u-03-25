2f94b3aa1dd1894d52a211c41d71b5aad601d708
lily <lily.you@student.tdsb.on.ca>
2025-02-21 18:37:29 +0000

    Completed Review 10

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,60 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
+		String q1 = pyramid(4);
+		System.out.println(q1);
+		String q2 = square(4);
+		System.out.println(q2);
+		boolean q3 = hasLowercase("Paul");
+		System.out.println(q3);
 	}
 
+	public static String pyramid(int rows){
+		String result = "";
+		for(int i = 1; i <= rows; i++){
+			for(int j = i; j < rows; j++){
+				result += " ";
+			}
+			for(int x = 0; x < i; x++){
+				if(x > 0){
+					result += " ";
+				}
+				result += "*";
+			}
+			if (i < rows) {
+                result += "\n";
+            }
+		}
+		return result;
+	}
+	
+	public static String square(int rows){
+		String result = "";
+		for(int i = 0; i < rows; i++){
+			for(int j = 0; j < rows; j++){
+				if(i < rows - 1 && i > 0 && j < rows -1 && j > 0){
+					result += " ";
+				}
+				else {
+					result += "*";
+				}
+			}
+			if (i < rows-1) {
+                result += "\n";
+            }
+		}
+		return result;
+	}
+
+	public static boolean hasLowercase(String word){
+		boolean result = false;
+		int wordInt;
+		for(int i = 0; i < word.length(); i++){
+			wordInt = (int)(word.charAt(i));
+			if(wordInt >= 97 && wordInt <= 122){
+				result = true;
+			}
+		}
+		return result;
+	}
 }
